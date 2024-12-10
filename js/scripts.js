4423363085bd8525c5d6a8be84c2750dd5b5541e
Antoine Macheda <antoine.macheda@ynov.com>
2024-12-10 13:13:56 +0100

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function showDetails(cliniqueId) {
+    // Hide all details sections
+    const details = document.querySelectorAll('.details-content');
+    details.forEach(detail => {
+      detail.style.display = 'none';
+    });
+  
+    // Show the selected details section
+    document.getElementById(cliniqueId).style.display = 'block';
+  
+    // Update the image
+    const image = document.getElementById('image-clinique');
+    image.src = `images/${cliniqueId}.png`;
+  }
+  
